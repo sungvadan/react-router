384462a2381b038280deb8bb44d6cad015460c1c
Van Truong PHAN <svd.phan@gmail.com>
2019-06-05 13:42:48 +0200

    add navigation using link

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ import * as serviceWorker from './serviceWorker';
 const routing = (
   <Router>
     <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/users'>Users</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
       <Route exact path="/" component={App}></Route>
       <Route path="/users" component={Users}></Route>
       <Route path="/contact" component={Contact}></Route>
