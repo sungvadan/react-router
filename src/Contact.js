ba2e2f2527be22962a18c7c1bfe36f2a3f4a231e
Van Truong PHAN <svd.phan@gmail.com>
2019-06-05 15:36:13 +0200

    redirect

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
 
 export default class Contact extends Component{
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.history.push('/')
+  }
+
   render() {
     return(
-      <h1>Contact</h1>
+      <form>
+        <h1>Contact</h1>
+        <input placeholder="name" type="name" />
+        <input placeholder="email" type="email" />
+        <button onClick={this.onSubmit}>Submit</button>
+      </form>
     )
   }
 }
